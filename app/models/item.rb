4932e9387e3769245d848c72a3abf2d1bf9e145b
mihomatsui <mihomatsui.24@gmail.com>
2021-02-27 10:57:37 +0900

    usersテーブルとitemsテーブルのアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  # itemは1人のユーザーに所属する ので、userは単数形
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # userはたくさんのitemを持っている
+  has_many :items
 end
