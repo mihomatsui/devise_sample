d1d810eb4a70def550f6efe91fae58a807fb26d9
mihomatsui <mihomatsui.24@gmail.com>
2021-02-27 12:18:38 +0900

    routeのコメントを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
   resources :items
-  # idを渡す必要がないのでresousceとする
+  # idを渡す必要がないのでresourceとする
   resource :users, only: :show
 end
